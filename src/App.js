107f1e58890f71287335c38876f9823cb509e8c1
mehmet dogan <mehmetseymen91@gmail.com>
2024-04-04 23:49:42 +0300

    impoer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React, {useEffect, useState} from 'react';
+import "./App.css";
+import Navbar from "./components/navbar";
+
+
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <React.Fragment>
+     <Navbar/>
+    </React.Fragment>
   );
 }
 
