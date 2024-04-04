2f8016efb79267a235fc605064db07f030fee0a9
mehmet dogan <mehmetseymen91@gmail.com>
2024-04-04 23:53:00 +0300

    navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+      <div className='nav_box'>
+        <span className='my_shop'>My Shopping</span>
+        <div className='cart'>
+            <span>
+                
+            </span>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default navbar
