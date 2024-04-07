8b9893e9acdccc869e6ba51de05b466b2cb85802
mehmet dogan <mehmetseymen91@gmail.com>
2024-04-07 23:14:32 +0300

    cards

--- src/components/amazon.jsx
+++ src/components/amazon.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import list from "../data";
 import Cards from "./card";
+import "../styles/amazon.css";
 
 
 const amazon = () => {
   return (
     <section>
-      {list.map((item) => {
-        <Cards/>
-      })}
+      {list.map((item) => (
+        <Cards key={item.id} item={item}/>
+      ))}
       
     </section>
   )
---
+++ src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <h2>
+      
+    </h2>
+  )
+}
+
+export default card
