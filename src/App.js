518032062fc71b266a8fcf9d1e255061774acdc8
mehmet dogan <mehmetseymen91@gmail.com>
2024-04-09 23:35:28 +0300

    cart handleChange

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Amazon from "./components/amazon";
 import Cart from "./components/cart";
 
-
-
 function App() {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
 
-const [show,setShow]= useState(true);
-const [cart,setCart]= useState([]);
-
-const handleClick = (item) => {
-  if(cart.indexOf(item) !== -1) return;
-  setCart([...cart,item])
-}
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+  const handleChange = (item, d) => {
+    const ind = card.indexOf(item);
+    const arr = cart;
+    arr[ind].amoount += d;
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
 
   return (
     <React.Fragment>
-     <Navbar setshow={setShow}/>
-     { show ? <Amazon/> : <Cart/>}
-     {/* <Amazon handleClick={handleClick}/> */}
+      <Navbar setshow={setShow} size={cart.length} />
+      {show ? (
+        <Amazon />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
+      {/* <Amazon handleClick={handleClick}/> */}
     </React.Fragment>
   );
 }
--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const cart = () => {
   return (
-    <div>
-      
-    </div>
+    <h2>
+      Card
+    </h2>
   )
 }
 
